d2a7ad7612ab45d87db5dde6bc81b99c72e5be25
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-11-11 08:45:18 +0100

    add query to magento_comments, fill array with data

--- magento_comments.rb
+++ magento_comments.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'mysql2'
+require 'tty-prompt'
 
 options = {}
 
@@ -22,5 +24,47 @@ if options[:databasename].to_s.strip == ''
 end
 
 puts "options #{options}"
+
+prompt = TTY::Prompt.new
+
+ShopwareReview = Struct.new(:articleID,
+                            :name,
+                            :headline,
+                            :comment,
+                            :points,
+                            :datum,
+                            :active,
+                            :sku,
+                            keyword_init: true)
+
+query = <<~SQL
+  SELECT v.vote_id, v.remote_ip, rating.rating_code, v.rating_id, v.review_id, v.percent, v.value, sku, title, detail, nickname, review.created_at
+  FROM rating_option_vote v
+  LEFT JOIN rating_option ON v.option_id = rating_option.option_id
+  LEFT JOIN rating ON rating_option.rating_id = rating.rating_id
+  LEFT JOIN review_detail ON v.review_id = review_detail.review_id
+  LEFT JOIN review ON v.review_id = review.review_id
+  LEFT JOIN catalog_product_entity on v.entity_pk_value = catalog_product_entity.entity_id
+  WHERE review.status_id = 1
+  LIMIT 5;
+SQL
+
+mysql_client = Mysql2::Client.new host: 'localhost', username: options[:username], password: options[:password], database: options[:databasename]
+
+reviews = mysql_client.query(query, symbolize_keys: true).map do |row|
+  ShopwareReview.new(
+    articleID: nil,
+    headline: row[:title],
+    comment:  row[:details],
+    points:   row[:value], # percent / 20
+    datum:    row[:created_at],
+    active:   1, # query should only fetch active review/ratings,
+    sku:      row[:sku]
+  )
+end
+
+reviews.each do |review|
+  prompt.yes?("Import #{review.inspect}")
+end
 
 exit 0
